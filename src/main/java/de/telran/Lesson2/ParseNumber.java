322e3290789be3bd3071051fb9daed2ab87493cf
dzxd16 <dzxd16@gmail.com>
2023-09-01 12:52:57 +0300

    Lesson2

---
+++ src/main/java/de/telran/Lesson2/ParseNumber.java
@@ -0,0 +1,13 @@
+package de.telran.Lesson2;
+
+public class ParseNumber {
+    public static void main(String[] args){
+        int myNum = 345;
+        System.out.println(myNum % 10);
+        System.out.println(myNum /10 % 10);
+        System.out.println(myNum /100);
+
+    }
+
+
+}
